9f40652a17ea3b089c373c2e11d80fbc892bd479
Faizan <faizansep5@gmail.com>
2021-05-22 09:09:47 +0530

    Initial commit

---
+++ src/components/CocktailList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
+
+const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        No cocktails matched your search criteria
+      </h2>
+    );
+  }
+  return (
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <h2 className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </h2>
+    </section>
+  );
+};
+
+export default CocktailList;
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        This is a React based project in which you can search for a cocktail and
+        check the details about that specific cocktail.
+      </p>
+    </section>
+  );
+};
+
+export default About;
